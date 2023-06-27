1ccf51d7275004fe82a825a17b515533c980adbe
Ryan DowlingSoka <ryan@dowlingsoka.com>
2023-06-26 22:25:26 -0700

    Delete erroneously submitted python files.

--- Content/Python/joe_snippets.py
+++
@@ -1,9 +0,0 @@
-import unreal
-from typing import List, cast
-
-eul = unreal.EditorUtilityLibrary
-assets = cast(List[unreal.Object], eul.get_selected_assets())
-for asset in assets:
-    if isinstance(asset, unreal.StaticMesh):
-        platforms = {"Windows": 0, "Switch": 1}
-        asset.set_minimum_lod_for_platforms(platforms)
